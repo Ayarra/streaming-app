6e9b098ec1251720b22f054bac67ac125e3fc530
ayarra <hamdaouihoussam.97@gmail.com>
2024-03-20 14:25:51 -0400

    server-feat: get a movie streamed

--- server/src/routes/moviesRoutes.js
+++ server/src/routes/moviesRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const torrentController = require("../controllers/moviesControllers");
+const test = require("../services/torrentServices");
 
+router.get("/stream", test.stream);
 router.get("/:id", torrentController.getMovie);
 router.get("/", torrentController.getMovies);
 
